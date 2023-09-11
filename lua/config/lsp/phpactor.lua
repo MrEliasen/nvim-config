d9d179d71ca52a5dd4c8970a88f470c43ce0b805
Mark Eliasen <mark.eliasen@pm.me>
2023-09-11 11:30:04 +0100

    add sleep and phpstan options to php actor

--- lua/config/lsp/phpactor.lua
+++ lua/config/lsp/phpactor.lua
@@ -1,3 +1,8 @@
 import("phpactor", function(actor)
-    actor.setup()
+    actor.setup({
+        init_options = {
+            ["language_server_phpstan.enabled"] = true,
+            ["language_server.diagnostic_sleep_time"] = 100,
+        }
+    })
 end)
