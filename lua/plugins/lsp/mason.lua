d827d79a0e3b94dd9871cf520bd4258b3cab16c1
Mark Eliasen <mark.eliasen@pm.me>
2024-08-26 14:18:03 +0100

    feat: add godot LSP support

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,6 +1,15 @@
 local mason = require("mason")
 local mason_lsp = require("mason-lspconfig")
 local cmp_lsp = require("cmp_nvim_lsp")
+local lspconfig = require('lspconfig')
+
+lspconfig.gdscript.setup {
+    force_setup = true, -- because the LSP is global. Read more on lsp-zero docs about this.
+    single_file_support = false,
+    -- cmd = {'ncat', '127.0.0.1', '6008'}, -- the important trick for Windows!
+    root_dir = require('lspconfig.util').root_pattern('project.godot', '.git'),
+    filetypes = {'gd', 'gdscript', 'gdscript3' }
+}
 
 mason.setup({
     ui = {
