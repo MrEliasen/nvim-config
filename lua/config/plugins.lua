f65b5805825f6e8afd73e8acea8aab55517c68dd
Mark Eliasen <mark.eliasen@pm.me>
2024-08-26 14:18:20 +0100

    chore: update telescope version

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -14,7 +14,7 @@ return {
 
 	-- telescope
     {
-        'nvim-telescope/telescope.nvim', tag = '0.1.5', dependencies = { 'nvim-lua/plenary.nvim' }
+        'nvim-telescope/telescope.nvim', tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' }
     },
     {
         'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release'
@@ -46,10 +46,6 @@ return {
     --[[ {
         "github/copilot.vim"
     }, ]]
-    {
-        "neovim/nvim-lspconfig",
-        lazy = false,
-    },
     {
         "aznhe21/actions-preview.nvim",
     },
