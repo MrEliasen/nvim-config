a31f1eebd96ed85d99d5fdee487199ae2328dad8
Mark Eliasen <mark.eliasen@pm.me>
2025-01-07 14:30:30 +0000

    i need some hover colour

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,39 +1,56 @@
 require("catppuccin").setup({
-	flavour = "mocha", -- latte, frappe, macchiato, mocha
-	term_colors = true,
-	transparent_background = false,
-	no_italic = false,
-	no_bold = false,
-	styles = {
-	    comments = {},
-	    conditionals = {},
-	    loops = {},
-	    functions = {},
-	    keywords = {},
-	    strings = {},
-	    variables = {},
-	    numbers = {},
-	    booleans = {},
-	    properties = {},
-	    types = {},
-	},
-	color_overrides = {
-	    mocha = {
-    		base = "#000000",
-	    },
-	},
-	highlight_overrides = {
-	    mocha = function(C)
-		return {
-		 --   TabLineSel = { bg = C.pink },
-		    NvimTreeNormal = { bg = C.none },
-		    CmpBorder = { fg = C.surface2 },
-		    Pmenu = { bg = C.none },
-		    NormalFloat = { bg = C.none },
-		    TelescopeBorder = { link = "FloatBorder" },
-		}
-	    end,
-	},
+    flavour = "mocha", -- latte, frappe, macchiato, mocha
+    term_colors = true,
+    transparent_background = false,
+    no_italic = false,
+    no_bold = false,
+    color_overrides = {
+        mocha = {
+            base = "#000000",
+            base2 = "#3b4261",
+        },
+    },
+    highlight_overrides = {
+        mocha = function(C)
+            return {
+                --   TabLineSel = { bg = C.pink },
+                NvimTreeNormal = { bg = C.none },
+                TelescopeBorder = { link = "FloatBorder" },
+                CmpBorder = { fg = C.surface2 },
+                Pmenu = { bg = C.none },
+
+                NormalFloat = { bg = C.base2, fg = C.text }, -- Set a solid background for floating windows
+                FloatBorder = { bg = C.base2, fg = C.surface2 }, -- Set border colors for float
+                LspSignatureActiveParameter = { bg = C.mantle, fg = C.text }, -- Highlight active signature parameter
+            }
+        end,
+    },
+    integrations = {
+        cmp = true,
+        nvimtree = true,
+        treesitter = true,
+        lsp_trouble = true,
+        native_lsp = {
+            enabled = true,
+            virtual_text = {
+                errors = { "italic" },
+                hints = { "italic" },
+                warnings = { "italic" },
+                information = { "italic" },
+                ok = { "italic" },
+            },
+            underlines = {
+                errors = { "underline" },
+                hints = { "underline" },
+                warnings = { "underline" },
+                information = { "underline" },
+                ok = { "underline" },
+            },
+            inlay_hints = {
+                background = true,
+            },
+        },
+    },
 })
 
 vim.cmd.colorscheme("catppuccin")
