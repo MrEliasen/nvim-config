158abdee97b7c34d261133cde5b14140c2e46185
Mark Eliasen <mark.eliasen@pm.me>
2026-02-13 13:47:01 +0000

    chore: formatting

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -4,58 +4,70 @@ local grp = aug("user_filetype_indent", { clear = true })
 
 -- Odin
 aucmd({ "BufRead", "BufNewFile" }, {
-    group = grp,
-    pattern = "*.odin",
-    callback = function()
-        vim.bo.filetype = "odin"
-    end,
+	group = grp,
+	pattern = "*.odin",
+	callback = function()
+		vim.bo.filetype = "odin"
+	end,
 })
 
 -- Laravel Blade
 aucmd({ "BufRead", "BufNewFile" }, {
-    group = grp,
-    pattern = "*.blade.php",
-    callback = function()
-        vim.bo.filetype = "blade"
-        vim.bo.shiftwidth = 4
-        vim.bo.tabstop = 4
-        vim.bo.softtabstop = 4
-        vim.bo.expandtab = true
-    end,
+	group = grp,
+	pattern = "*.blade.php",
+	callback = function()
+		vim.bo.filetype = "blade"
+		vim.bo.shiftwidth = 4
+		vim.bo.tabstop = 4
+		vim.bo.softtabstop = 4
+		vim.bo.expandtab = true
+	end,
 })
 
 -- 2-space ecosystems
 aucmd("FileType", {
-    group = grp,
-    pattern = { "dart", "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "json", "yaml", "css", "scss", "html" },
-    callback = function()
-        vim.bo.shiftwidth = 2
-        vim.bo.tabstop = 2
-        vim.bo.softtabstop = 2
-        vim.bo.expandtab = true
-    end,
+	group = grp,
+	pattern = {
+		"dart",
+		"javascript",
+		"javascriptreact",
+		"typescript",
+		"typescriptreact",
+		"vue",
+		"json",
+		"yaml",
+		"css",
+		"scss",
+		"html",
+	},
+	callback = function()
+		vim.bo.shiftwidth = 2
+		vim.bo.tabstop = 2
+		vim.bo.softtabstop = 2
+		vim.bo.expandtab = true
+	end,
 })
 
 -- Go
 aucmd("FileType", {
-    group = grp,
-    pattern = { "go" },
-    callback = function()
-        vim.bo.expandtab = false
-        vim.bo.tabstop = 8
-        vim.bo.shiftwidth = 0
-        vim.bo.softtabstop = 0
-    end,
+	group = grp,
+	pattern = { "go" },
+	callback = function()
+		vim.bo.expandtab = false
+		vim.bo.tabstop = 4
+		vim.bo.shiftwidth = 4
+		vim.bo.softtabstop = 4
+	end,
 })
 
 -- PHP (4 spaces)
 aucmd("FileType", {
-    group = grp,
-    pattern = { "php", "blade" },
-    callback = function()
-        vim.bo.shiftwidth = 4
-        vim.bo.tabstop = 4
-        vim.bo.softtabstop = 4
-        vim.bo.expandtab = true
-    end,
+	group = grp,
+	pattern = { "php", "blade" },
+	callback = function()
+		vim.bo.shiftwidth = 4
+		vim.bo.tabstop = 4
+		vim.bo.softtabstop = 4
+		vim.bo.expandtab = true
+	end,
 })
--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,9 +1,9 @@
 local function root(patterns)
-    return function(bufnr, cb)
-        local fname = vim.api.nvim_buf_get_name(bufnr)
-        local r = vim.fs.root(fname, patterns)
-        cb(r or vim.fn.getcwd())
-    end
+	return function(bufnr, cb)
+		local fname = vim.api.nvim_buf_get_name(bufnr)
+		local r = vim.fs.root(fname, patterns)
+		cb(r or vim.fn.getcwd())
+	end
 end
 
 -- Common client capabilities (works fine without cmp; extend later if you add cmp)
@@ -11,116 +11,132 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 
 -- LUA
 vim.lsp.config("lua_ls", {
-    cmd = { "lua-language-server" },
-    filetypes = { "lua" },
-    root_dir = root({ ".git", ".luarc.json", ".luarc.jsonc" }),
-    settings = {
-        Lua = {
-            diagnostics = { globals = { "vim" } },
-            workspace = { checkThirdParty = false },
-            telemetry = { enable = false },
-        },
-    },
-    capabilities = capabilities,
+	cmd = { "lua-language-server" },
+	filetypes = { "lua" },
+	root_dir = root({ ".git", ".luarc.json", ".luarc.jsonc" }),
+	settings = {
+		Lua = {
+			diagnostics = { globals = { "vim" } },
+			workspace = { checkThirdParty = false },
+			telemetry = { enable = false },
+		},
+	},
+	capabilities = capabilities,
 })
 vim.lsp.enable("lua_ls")
 
 -- GO
 vim.lsp.config("gopls", {
-    cmd = { "gopls" },
-    filetypes = { "go", "gomod", "gosum", "gotmpl" },
-    root_dir = root({ "go.work", "go.mod", ".git" }),
-    settings = {
-        gopls = {
-            gofumpt = true,
-            staticcheck = true,
-            analyses = {
-                unusedparams = true,
-                nilness = true,
-                unusedwrite = true,
-                useany = true,
-            },
-            hints = {
-                assignVariableTypes = true,
-                compositeLiteralFields = true,
-                compositeLiteralTypes = true,
-                constantValues = true,
-                functionTypeParameters = true,
-                parameterNames = true,
-                rangeVariableTypes = true,
-            },
-        },
-    },
-    capabilities = capabilities,
+	cmd = { "gopls" },
+	filetypes = { "go", "gomod", "gosum", "gotmpl" },
+	root_dir = root({ "go.work", "go.mod", ".git" }),
+	settings = {
+		gopls = {
+			gofumpt = true,
+			staticcheck = true,
+			analyses = {
+				unusedparams = true,
+				nilness = true,
+				unusedwrite = true,
+				useany = true,
+			},
+			hints = {
+				assignVariableTypes = true,
+				compositeLiteralFields = true,
+				compositeLiteralTypes = true,
+				constantValues = true,
+				functionTypeParameters = true,
+				parameterNames = true,
+				rangeVariableTypes = true,
+			},
+		},
+	},
+	capabilities = capabilities,
 })
 vim.lsp.enable("gopls")
 
 -- PHP / Laravel
 vim.lsp.config("intelephense", {
-    cmd = { "intelephense", "--stdio" },
-    filetypes = { "php", "blade" },
-    root_dir = root({ "composer.json", "artisan", ".git" }),
-    settings = {
-        intelephense = {
-            files = {
-                maxSize = 5000000,
-            },
-            format = { enable = false },
-            completion = {
-                insertUseDeclaration = true,
-                triggerParameterHints = true,
-            },
-        },
-    },
-    capabilities = capabilities,
+	cmd = { "intelephense", "--stdio" },
+	filetypes = { "php", "blade" },
+	root_dir = root({ "composer.json", "artisan", ".git" }),
+	settings = {
+		intelephense = {
+			files = {
+				maxSize = 5000000,
+			},
+			format = { enable = false },
+			completion = {
+				insertUseDeclaration = true,
+				triggerParameterHints = true,
+			},
+		},
+	},
+	capabilities = capabilities,
 })
 vim.lsp.enable("intelephense")
 
 vim.lsp.config("phpactor", {
-    cmd = { 'phpactor', 'language-server' },
-    filetypes = { 'php', 'blade' },
-    root_dir = root({ "composer.json", ".git" }),
-    root_markers = { '.git', 'composer.json', '.phpactor.json', '.phpactor.yml' },
-    workspace_required = true,
-    init_options = {
-        ["language_server_phpstan.enabled"] = false,
-        ["language_server_psalm.enabled"] = false,
-    },
-    capabilities = capabilities,
+	cmd = { "phpactor", "language-server" },
+	filetypes = { "php", "blade" },
+	root_dir = root({ "composer.json", ".git" }),
+	root_markers = { ".git", "composer.json", ".phpactor.json", ".phpactor.yml" },
+	workspace_required = true,
+	init_options = {
+		["language_server_phpstan.enabled"] = false,
+		["language_server_psalm.enabled"] = false,
+	},
+	capabilities = capabilities,
 })
 --vim.lsp.enable("phpactor")
 
 -- Odin
 vim.lsp.config("ols", {
-    cmd = { "ols" },
-    filetypes = { "odin" },
-    root_dir = root({ ".git" }),
-    capabilities = capabilities,
+	cmd = { "ols" },
+	filetypes = { "odin" },
+	root_dir = root({ "ols.json", "odinfmt.json" }),
+	init_options = {
+		enable_semantic_tokens = true,
+		enable_document_symbols = true,
+		newline_limit = 2,
+		character_width = 100,
+		enable_hover = true,
+		enable_snippets = true,
+		enable_references = true,
+		enable_inlay_hints_params = true,
+		enable_inlay_hints_default_params = true,
+		enable_inlay_hints_implicit_return = true,
+		enable_checker_only_saved = false,
+		enable_auto_import = true,
+		checker_args = "-vet -vet-cast -vet-semicolon -vet-using-param",
+		collections = {},
+	},
+	capabilities = capabilities,
 })
 vim.lsp.enable("ols")
 
 -- TS/JS (vtsls)
 vim.lsp.config("vtsls", {
-    cmd = { "vtsls", "--stdio" },
-    filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-    root_dir = root({ "package.json", "tsconfig.json", "jsconfig.json", ".git" }),
-    capabilities = capabilities,
+	cmd = { "vtsls", "--stdio" },
+	filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+	root_dir = root({ "package.json", "tsconfig.json", "jsconfig.json", ".git" }),
+	capabilities = capabilities,
 })
 vim.lsp.enable("vtsls")
 
 -- Vue (Volar / vue-language-server)
 vim.lsp.config("vue_ls", {
-    cmd = { "vue-language-server", "--stdio" },
-    filetypes = { "vue" },
-    root_dir = root({ "package.json", "tsconfig.json", "jsconfig.json", ".git" }),
-    capabilities = capabilities,
+	cmd = { "vue-language-server", "--stdio" },
+	filetypes = { "vue" },
+	root_dir = root({ "package.json", "tsconfig.json", "jsconfig.json", ".git" }),
+	capabilities = capabilities,
 })
 vim.lsp.enable("vue_ls")
 
 -- JSON (jsonls)
 vim.lsp.config("jsonls", {
-    cmd = { "json-language-server", "--stdio" },
-    filetypes = { "json", "jsonc" },
-    capabilities = capabilities,
+	cmd = { "json-language-server", "--stdio" },
+	filetypes = { "json", "jsonc" },
+	capabilities = capabilities,
 })
 vim.lsp.enable("jsonls")
