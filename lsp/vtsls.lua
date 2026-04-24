f34327825c0b61033c78fa7775fd8623b098cdee
Mark Eliasen <mark.eliasen@pm.me>
2026-04-24 09:55:19 +0100

    fix vue

--- lsp/vtsls.lua
+++ lsp/vtsls.lua
@@ -1,5 +1,29 @@
+local vue_plugin = vim.fn.stdpath("data")
+	.. "/mason/packages/vue-language-server/node_modules/@vue/typescript-plugin"
+
 return {
 	cmd = { "vtsls", "--stdio" },
-	filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+	filetypes = {
+		"javascript",
+		"javascriptreact",
+		"typescript",
+		"typescriptreact",
+		"vue",
+	},
 	root_markers = { "package.json", "tsconfig.json", "jsconfig.json", ".git" },
+	settings = {
+		vtsls = {
+			tsserver = {
+				globalPlugins = {
+					{
+						name = "@vue/typescript-plugin",
+						location = vue_plugin,
+						languages = { "vue" },
+						configNamespace = "typescript",
+						enableForWorkspaceTypeScriptVersions = true,
+					},
+				},
+			},
+		},
+	},
 }
--- lsp/vue_ls.lua
+++ lsp/vue_ls.lua
@@ -2,4 +2,26 @@ return {
 	cmd = { "vue-language-server", "--stdio" },
 	filetypes = { "vue" },
 	root_markers = { "package.json", "tsconfig.json", "jsconfig.json", ".git" },
+	on_init = function(client)
+		client.handlers["tsserver/request"] = function(_, result, context)
+			local clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = "vtsls" })
+			if #clients == 0 then
+				vim.notify(
+					"vtsls LSP client not found. vue_ls needs vtsls for script/template type info.",
+					vim.log.levels.ERROR
+				)
+				return
+			end
+			local ts_client = clients[1]
+			local param = unpack(result)
+			local id, command, payload = unpack(param)
+			ts_client:exec_cmd({
+				title = "vue_request_forward",
+				command = "typescript.tsserverRequest",
+				arguments = { command, payload },
+			}, { bufnr = context.bufnr }, function(_, r)
+				client:notify("tsserver/response", { { id, r.body } })
+			end)
+		end
+	end,
 }
