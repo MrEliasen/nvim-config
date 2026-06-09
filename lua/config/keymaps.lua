cc414b645cfef9d07f7ffb77db4121f3f8a7f91b
Mark Eliasen <mark.eliasen@pm.me>
2026-06-09 10:30:15 +0100

    fix node switching in toggle term

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -20,3 +20,11 @@ map("v", "K", ":m '<-2<CR>gv=gv", opts)
 -- Navigate buffers
 map("n", "<S-h>", ":BufferLineCyclePrev<CR>", opts)
 map("n", "<S-l>", ":BufferLineCycleNext<CR>", opts)
+
+-- Terminal mode -> Normal mode (so you can scroll / page / copy).
+-- <C-\> is the ToggleTerm toggle, so it eats the native <C-\><C-n>; use Esc.
+-- Then: <C-u>/<C-d> scroll, PageUp/PageDown page, v + y to copy, i/a to type again.
+map("t", "<Esc>", [[<C-\><C-n>]], opts)
+-- Page directly while still in the shell (drops to Normal mode, then pages).
+map("t", "<PageUp>", [[<C-\><C-n><C-b>]], opts)
+map("t", "<PageDown>", [[<C-\><C-n><C-f>]], opts)
